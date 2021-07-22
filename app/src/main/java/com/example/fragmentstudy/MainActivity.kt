d5721056c4f0a451ac3814d5cd2e2e19d56a6403
KimHaNeul <s2025@e-mirim.hs.kr>
2021-07-22 09:34:54 +0900

    프래그먼트 값 MainActivity.kt로 반환 구현

--- app/src/main/java/com/example/fragmentstudy/MainActivity.kt
+++ app/src/main/java/com/example/fragmentstudy/MainActivity.kt
@@ -5,12 +5,22 @@ import android.os.Bundle
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity(),CurrencyConverterFragment3.CurrencyCalculationListener {
+//    # 프래그먼트 정리
+//
+//    * 프래그먼트로 값을 전달하려면 => newInstance 클래스 메소드 구현하고 그곳으로 값을 전달 후 번들을 통해 프래그먼트로 값 전달 (arguments 속성 대입)
+//    * 프래그먼트에서 값을 반환받으려면 => 호스트 액티비티 측에서 인터페이스 구현 후, 프래그먼트 측에서는 해당 인터페이스를 구현하고 있는 액티비티로 추상 메소드를 호출하며 값을 전달
 
 
-    override fun onCalculate(result: Double, amount: Double, from: String, to: String) {
-        Toast.makeText(this,"${result} ${from} ${to}",Toast.LENGTH_SHORT).show()
+    override fun onCalculate(result: Double,
+                             amount: Double,
+                             from: String,
+                             to: String) {
+        Toast.makeText(this, "${result} ${from} ${to}",
+            Toast.LENGTH_SHORT)
+            .show()
     }
-    
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,12 +31,17 @@ class MainActivity : AppCompatActivity(),CurrencyConverterFragment3.CurrencyCalc
         //첫번째 인자 : 프래그먼트가 부착될 뷰그룹의 식별자
         //두번째 인자 : 프래그먼트 객체
         //transaction.add(R.id.fragment_container,CurrencyConverterFragment1())
-
+/*
         transaction.add(R.id.fragment_container,
             CurrencyConverterFragment2.newInstance("USD", "KRW"))
         transaction.add(R.id.fragment_container,
             CurrencyConverterFragment2.newInstance("JPY", "KRW"))
+*/
+        transaction.add(R.id.fragment_container,
+            CurrencyConverterFragment3.newInstance("USD", "KRW"))
 
+        transaction.add(R.id.fragment_container,
+            CurrencyConverterFragment3.newInstance("JPY", "KRW"))
 
         transaction.commit()
     }
